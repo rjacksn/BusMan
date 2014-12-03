91507e979dd3aae73c17820d373d08dcc91ccdc6
Richard Jackson <Rge.Jackson@gmail.com>
2014-12-02 20:17:18 -0500

    UI Updates

--- www/js/statusPage.js
+++ www/js/statusPage.js
@@ -1,4 +1,4 @@
-﻿var statusPage = {
+var statusPage = {
     status_page: null,
     status_list: null,
     _url: null,
@@ -27,32 +27,34 @@
         $.get(this._url, {}, function (json)
         {
             statusPage.status_list.innerHTML = "";
-            var html = '<ul id="bus_info_list">';
+            var html = '<div class="bus_info_list">';
             console.log(JSON.stringify(json));
             //statusPage.status_list.innerHTML = json;
             var stops = json.stops;
             for (var i = 0; i < stops.length; i++) {
                 var stop = stops[i];
-                html += "<li>stop_no:" + stop.stop_no + "</li>";
-                html += "<li>stop_name:" + stop.stop_name + "</li>";
+                
+                html += "<h2 class='bus_stop'>" + stop.stop_no + " - " + stop.stop_name + "</h2>";
+                html += "<div class='bus_route'>";
                 var routes = stop.routes;
                 for (var j = 0; j < routes.length; j++) {
                     var route = routes[j];
-                    html += "<li>route_no:" + route.route_no + "</li>";
                     var trips = route.trips;
                     if (trips.length == 0) {
-                        html += "<li>This route is not running at this time.</li>";
+                        html += "<img src='img/oc_bus_button.png' class='route_icon'/><h3 class='bus_title'>" + route.route_no + "</h3>";
+                        html += "<h4 class='no_bus'>This route is not running at this time.</h4>";
                         count++;
                     }
                     else {
                         for (var k = 0; k < trips.length; k++) {
                             count++;
                             var trip = trips[k];
-                            html += "<li>Direction:" + trip.Direction + "</li>";
-                            html += "<li>RouteLabel:" + trip.RouteLabel + "</li>";
+                            html += "<div class='route_block'><img src='img/oc_bus_button.png' class='route_icon'/><h3 class='bus_title'>" + route.route_no + " - " + trip.RouteLabel + "</h3></div>";
+                            //html += "<h4>" + trip.Direction + "</h4>";
+                            
                             var tripsForStop = trip.tripsForStop;
                             if (tripsForStop.length == 0) {
-                                html += "<li>There are no scheduled buses at this time.</li>";
+                                html += "<h4 class='no_bus'>There are no scheduled buses at this time.</h4>";
                             }
                             else {
                                 for (var l = 0; l < tripsForStop.length; l++) {
@@ -68,15 +70,16 @@
                                         var arrival = new Date(now.getTime() + tripForStop.AdjustedScheduleTime * 60000);
                                         arrivalTime = arrival.toLocaleTimeString();
                                     }
-                                    html += "<li>Arriving at: " + arrivalTime + "</li>";
+                                    html += "<div class='bus_arrival'><h4>Arriving at: " + arrivalTime + "</h4><button>Set Timer</button></div><br/>";
                                 }
                             }
                         }
                     }
 
                 }
+                html += "</div>";
             }
-            html += "</ul>";
+            html += "</div>";
             statusPage.status_list.innerHTML = html;
             google.maps.event.trigger(statusPage, "make_status_done", "SUCCESS");
 
